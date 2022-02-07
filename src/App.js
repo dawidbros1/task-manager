a7f2a960d5e7c5325ebb45b50187ec61a79f4d42
dawidbros1 <dawidbros1@wp.pl>
2022-02-07 16:07:37 +0100

    added StoreProvider

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 import Header from './components/Header';
 import HomePage from './components/HomePage/HomePage';
+import StoreProvider from './store/StoreProvider';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.scss';
@@ -9,14 +10,15 @@ import './App.scss';
 const App = () => {
   return (
     <div className="container-fluid">
-      <Router>
-        <Header isLogin={false} />
-
-        <Routes>
-          <Route exact path="/" element={<HomePage />} />
-          {/* <Route path="*" element={<NotFound/>}/> */}
-        </Routes>
-      </Router>
+      <StoreProvider>
+        <Router>
+          <Header isLogin={false} />
+          <Routes>
+            <Route exact path="/" element={<HomePage />} />
+            {/* <Route path="*" element={<NotFound/>}/> */}
+          </Routes>
+        </Router>
+      </StoreProvider>
     </div >
   );
 }
