a2ce98f024c6c9700940e5411a93ab7807f08234
dawidbros1 <dawidbros1@wp.pl>
2022-02-08 13:40:23 +0100

    added menu for logged user and logout method

--- src/components/Header/subcomponents/UserMenu.jsx
+++ src/components/Header/subcomponents/UserMenu.jsx
@@ -1,10 +1,33 @@
+import { useContext, useEffect, useState } from "react";
+import { StoreContext } from "../../../store/StoreProvider";
+
 const UserMenu = () => {
+   const [isMenuActive, setIsMenuActive] = useState(true);
+
+   const { user, setUser } = useContext(StoreContext)
+
+   const logout = () => setUser(null)
+
+   const handleToggleisMenuActive = () => setIsMenuActive((prev) => (!prev));
+
+   const closeMenu = ({ target: { id } }) => id !== "wrapper" && setIsMenuActive(false);
+
+   useEffect(() => {
+      window.addEventListener('click', closeMenu);
+      return () => (window.removeEventListener('click', closeMenu))
+   }, [])
 
    return (
       <>
-         UserMenu
+         <div className="mx-auto"></div>
+         <nav id="menu">
+            <div id="wrapper" onClick={handleToggleisMenuActive}>{user.username}</div>
+            <ul className={isMenuActive ? "d-block" : "d-none"}>
+               <li>Profil</li>
+               <li onClick={logout}>Wyloguj</li>
+            </ul>
+         </nav>
       </>
-
    );
 };
 
