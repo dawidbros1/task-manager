6dd51552621b0fa07d5e6190cd468ce2e1c77a39
dawidbros1 <dawidbros1@wp.pl>
2022-02-14 12:20:31 +0100

    new form component Textarea

---
+++ src/components/Form/TextArea.jsx
@@ -0,0 +1,21 @@
+const Textarea = ({ id, type, labelText, value, onChange, errors = {}, rules = [] }) => {
+
+   const errorsBlock = rules.map((rule) => (
+      errors.hasOwnProperty(rule) &&
+      <div key={`${rule}:`} className="error">
+         {errors[rule]}
+      </div>
+   ))
+
+   const isEmpty = Object.keys(errors).length === 0;
+
+   return (
+      <div className="mb-1 d-flex flex-wrap box p-2">
+         <label htmlFor={id}>{labelText}</label>
+         <textarea type={type} id={id} onChange={onChange} value={value} />
+         {!isEmpty ? <div className="errors">{errorsBlock}</div> : null}
+      </div>
+   );
+}
+
+export default Textarea;
