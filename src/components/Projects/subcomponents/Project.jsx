5858262bdbaed6f2675c9bfd655cb2f7a4d67c00
dawidbros1 <dawidbros1@wp.pl>
2022-02-14 14:45:20 +0100

    added form to edit project

--- src/components/Projects/subcomponents/Project.jsx
+++ src/components/Projects/subcomponents/Project.jsx
@@ -1,9 +1,32 @@
+import { useState } from "react";
+import ProjectForm from "./ProjectForm";
+
 const Project = ({ id, name, description }) => {
+
+   const [isEditProjectFormOpen, setIsEditProjectFormOpen] = useState(false);
+
+   const handleOpenEditProjectFrom = () => setIsEditProjectFormOpen(true);
+   const handleCloseEditProjectFrom = () => {
+      console.log("Projekt - EDIT")
+      setIsEditProjectFormOpen(false);
+   }
+
+   const editProjectFormComponent = isEditProjectFormOpen &&
+      <ProjectForm
+         handleOnClose={handleCloseEditProjectFrom}
+         id={id}
+         entryName={name}
+         entryDescription={description}
+         action="edit"
+      />;
+
    return (
       <div className="project">
          <div className="fw-bold">{name}</div>
          <p className="description">{description}</p>
-         <button className="edit">edytuj</button>
+         <button className="edit" onClick={handleOpenEditProjectFrom}>edytuj</button>
+
+         {editProjectFormComponent}
       </div>
    )
 }
