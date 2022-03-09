065cded66b9de019333e43325e6c207b320406ca
dawidbros1 <dawidbros1@wp.pl>
2022-03-09 10:06:54 +0100

    edit in homePage

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,9 +1,27 @@
 const HomePage = () => {
    return (
-      <div>
-         Strona główna
-         <img src="https://www.lasnazawsze.org.pl/wp-content/uploads/sites/52/2020/06/dsc3692-1-1024x684.jpg"></img>
-      </div>
+      <main>
+         <div className="text-center h2 border-bottom p-2 bg-primary text-white">Task Manager</div>
+
+         <p>
+            <div className="fw-bold">Czym jest nasz serwis?</div>
+            <div>Jest to serwis to zarządzania zadaniami w projekcie.</div>
+         </p>
+
+         <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, odit ullam! Quos perferendis iste ab, illum provident tempora nobis eveniet consequuntur pariatur tempore reprehenderit libero, voluptas neque optio ratione reiciendis.
+            At earum nemo illum sapiente, omnis laboriosam minima temporibus molestiae. Ipsum facere, distinctio aperiam eveniet pariatur aliquid, nostrum earum adipisci, minima corporis fugit perspiciatis rerum ea dignissimos ad quod porro?
+            Officia et error debitis iste praesentium perspiciatis nihil velit, explicabo officiis natus. Dolores, pariatur assumenda eligendi libero modi, facilis quas quisquam harum corrupti qui odit nulla suscipit laudantium, molestias nisi!
+            Maiores illo dolores, in voluptatum nulla necessitatibus deleniti commodi asperiores quis quasi alias ipsa dicta officia eaque molestiae eius laboriosam aperiam nihil aut et sapiente quam possimus. Quibusdam, animi maxime.
+            Libero ipsa inventore repellendus, iusto ducimus, at recusandae aliquam deleniti perspiciatis a consectetur ex, reiciendis quidem placeat voluptatem exercitationem. Soluta rem fuga unde! Quia corporis aperiam eos aspernatur. Veritatis, similique.
+            Facere id beatae debitis. Nobis explicabo veritatis possimus pariatur fugit autem sequi rem, ipsum optio sed iste provident eos culpa nesciunt, exercitationem aut inventore quae nihil debitis placeat soluta laudantium.
+            Eum et odit, neque laudantium consequuntur temporibus sequi voluptatem voluptatibus doloribus, quos dolore saepe, iusto impedit exercitationem voluptates nisi id omnis fuga perferendis porro non veritatis. Illum tempore doloremque ex?
+            Similique at ullam, optio ut consequuntur, vitae fugiat dolor ea illum explicabo porro nostrum suscipit aperiam quaerat in distinctio neque. Dolorem aliquid quisquam pariatur amet incidunt voluptatum tempora commodi voluptatem.
+            Totam at ad beatae perspiciatis assumenda eos. Cupiditate quos alias, quis molestias aut dolores saepe delectus quas sint mollitia harum quaerat voluptas magnam numquam? Quibusdam labore dolor repellendus repellat quae.
+            Voluptatibus perspiciatis quo id a, quasi ipsam blanditiis, harum exercitationem earum optio soluta pariatur autem deleniti sunt deserunt aut nostrum ut aliquid quia iure ad! Cum alias maiores soluta illum?
+         </p>
+
+      </main>
    );
 };
 
